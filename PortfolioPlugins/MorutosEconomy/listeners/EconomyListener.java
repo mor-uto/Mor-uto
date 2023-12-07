81039687619cd7db14d42ab80d571e11d0bd82b5
Moruto <79018613+Mor-uto@users.noreply.github.com>
2023-12-08 01:54:48 +0200

    Update EconomyListener.java

--- PortfolioPlugins/MorutosEconomy/listeners/EconomyListener.java
+++ PortfolioPlugins/MorutosEconomy/listeners/EconomyListener.java
@@ -1,6 +1,7 @@
 package net.moruto.economy.listeners;
 
 import net.moruto.economy.MorutosEconomy;
+import net.moruto.economy.utils.ConfigManager;
 import org.bukkit.GameMode;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -10,21 +11,23 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class EconomyListener implements Listener {
-    public List<String> method = List.of(MorutosEconomy.getInstance().getConfigManager().getMethod());
-    public int amount = MorutosEconomy.getInstance().getConfigManager().getAmount();
-
+    public List<String> method = MorutosEconomy.getInstance().getConfigManager().getMethod();
+    private final ConfigManager config = MorutosEconomy.getInstance().getConfigManager();
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
         Player player = event.getPlayer();
 
+        if (workChecksNo(player)) {
+            return;
+        }
+
         if (method.contains("blockbreaking")) {
             if (player.getGameMode().equals(GameMode.SURVIVAL)) {
-                MorutosEconomy.getInstance().economyImplementer.depositPlayer(player, amount);
+                MorutosEconomy.getInstance().economyImplementer.depositPlayer(player, config.getPerBlockAmount());
             }
         }
     }
@@ -32,19 +35,31 @@ public class EconomyListener implements Listener {
     @EventHandler
     private void onKill(EntityDamageByEntityEvent event) {
         if (event.getDamager() instanceof Player && !(event.getEntity() instanceof Player)) {
+            if (workChecksNo((Player) event.getDamager())) {
+                return;
+            }
+
             if (method.contains("mobkilling")) {
                 LivingEntity entity = (LivingEntity) event.getEntity();
                 if (entity.getHealth() == 0) {
-                    MorutosEconomy.getInstance().economyImplementer.depositPlayer((Player) event.getDamager(), amount);
+                    MorutosEconomy.getInstance().economyImplementer.depositPlayer((Player) event.getDamager(), config.getPerMobkillAmount());
                 }
             }
         }
     }
-    
+
     @EventHandler
     private void onPlayerMove(PlayerMoveEvent event) {
-        if (method.contains("moving")) {
-            MorutosEconomy.getInstance().economyImplementer.depositPlayer(event.getPlayer(), amount);
+        if (workChecksNo(event.getPlayer())) {
+            return;
         }
+
+        if (method.contains("moving")) {
+            MorutosEconomy.getInstance().economyImplementer.depositPlayer(event.getPlayer(), config.getPerBlockWalkAmount());
+        }
+    }
+
+    private boolean workChecksNo(Player player) {
+        return !config.getWorkingWorlds().isEmpty() && !config.getWorkingWorlds().contains(player.getWorld().getName().toLowerCase());
     }
 }

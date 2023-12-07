f1f01e72ab16458513a8fc8f911079bb4315bb4a
Moruto <79018613+Mor-uto@users.noreply.github.com>
2023-12-08 01:54:28 +0200

    Update ConfigManager.java

--- PortfolioPlugins/MorutosEconomy/utils/ConfigManager.java
+++ PortfolioPlugins/MorutosEconomy/utils/ConfigManager.java
@@ -2,22 +2,52 @@ package net.moruto.economy.utils;
 
 import net.moruto.economy.MorutosEconomy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConfigManager {
-    private final String[] method;
-    private final int amount;
+    private final List<String> method;
+    private final List<String> workingWorlds;
+    private final double perBlockAmount, perBlockWalkAmount, perMobkillAmount;
+    private final String prefix;
+    private final boolean usePrefix;
 
     public ConfigManager() {
-        if (MorutosEconomy.getInstance().getConfig().getString("moneyMethod").contains(",")) method = MorutosEconomy.getInstance().getConfig().getString("moneyMethod").split(",");
-        else method = new String[]{MorutosEconomy.getInstance().getConfig().getString("moneyMethod")};
+        String methodsBase = MorutosEconomy.getInstance().getConfig().getString("moneyMethod");
+        String workingWorldsBase = MorutosEconomy.getInstance().getConfig().getString("workingWorlds");
 
-        amount = MorutosEconomy.getInstance().getConfig().getInt("moneyPerKillOrBlockBreak");
+        method = new ArrayList<>();
+        workingWorlds = new ArrayList<>();
+
+        method.addAll(List.of(methodsBase.replaceAll(" ", "").split(",")));
+        workingWorlds.addAll(List.of(workingWorldsBase.replaceAll(" ", "").split(",")));
+
+        prefix = MorutosEconomy.getInstance().getConfig().getString("prefix");
+        usePrefix = MorutosEconomy.getInstance().getConfig().getBoolean("enablePrefix");
+        perBlockAmount = MorutosEconomy.getInstance().getConfig().getDouble("moneyPerKillOrBlockBreak");
+        perBlockWalkAmount = MorutosEconomy.getInstance().getConfig().getDouble("moneyPerBlockWalked");
+        perMobkillAmount = MorutosEconomy.getInstance().getConfig().getDouble("moneyPerMobKilled");
     }
 
-    public String[] getMethod() {
+    public List<String> getMethod() {
         return method;
     }
 
-    public int getAmount() {
-        return amount;
+    public List<String> getWorkingWorlds() {
+        return workingWorlds;
+    }
+
+    public double getPerBlockWalkAmount() {
+        return perBlockWalkAmount;
+    }
+    public double getPerBlockAmount() {
+        return perBlockAmount;
+    }
+    public double getPerMobkillAmount() {
+        return perMobkillAmount;
+    }
+
+    public String getPrefix() {
+        return (usePrefix) ? prefix : "";
     }
 }

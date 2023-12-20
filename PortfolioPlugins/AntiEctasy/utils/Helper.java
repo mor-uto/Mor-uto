eb31dfa4ee0922f15a58705c759c3cbb8945f853
Moruto <79018613+Mor-uto@users.noreply.github.com>
2023-12-20 17:37:01 +0200

    Create Helper.java

---
+++ PortfolioPlugins/AntiEctasy/utils/Helper.java
@@ -0,0 +1,9 @@
+package net.moruto.antiectasy.utils;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class Helper {
+    public static String trans(String str) {
+        return ChatColor.translateAlternateColorCodes('&', str);
+    }
+}

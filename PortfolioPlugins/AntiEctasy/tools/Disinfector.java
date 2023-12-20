300cbf1c67bbad132a13dd63a4a6d9a44fb2fd7c
Moruto <79018613+Mor-uto@users.noreply.github.com>
2023-12-20 17:36:42 +0200

    Create Disinfector.java

---
+++ PortfolioPlugins/AntiEctasy/tools/Disinfector.java
@@ -0,0 +1,8 @@
+package net.moruto.antiectasy.tools;
+
+public class Disinfector {
+    public boolean disinfect(String jarFilePath, String classNameToDelete) {
+        //working on it...
+        return true;
+    }
+}

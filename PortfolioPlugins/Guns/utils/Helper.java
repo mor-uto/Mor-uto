8c12398e589eba5a4fd7967d962800cf215a0433
Moruto <79018613+Mor-uto@users.noreply.github.com>
2024-01-14 00:38:58 +0200

    Delete PortfolioPlugins directory

--- PortfolioPlugins/Guns/utils/Helper.java
+++
@@ -1,9 +0,0 @@
-package net.moruto.guns.utils;
-
-import org.bukkit.ChatColor;
-
-public class Helper {
-    public static String trans(String str) {
-        return ChatColor.translateAlternateColorCodes('&', str);
-    }
-}
